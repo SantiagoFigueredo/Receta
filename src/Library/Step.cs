72a89587f5996f0ef6c60bad420876703248eb78
Santiago Figueredo <66336675+SantiagoFigueredo@users.noreply.github.com>
2023-05-03 20:54:54 -0300

    Se agrega el IJsonConvertible

--- src/Library/Step.cs
+++ src/Library/Step.cs
@@ -9,7 +9,7 @@ using System.Text.Json.Serialization;
 
 namespace Recipies
 {
-    public class Step
+    public class Step : IJsonConvertible
     {
         [JsonConstructor]
         public Step(Product input, double quantity, Equipment equipment, int time)
@@ -32,5 +32,18 @@ namespace Recipies
         public int Time { get; set; }
 
         public Equipment Equipment { get; set; }
+
+        public string ConvertToJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+
+        public void LoadFromJson(string json)
+        {
+            Step step = JsonSerializer.Deserialize<Step>(json);
+            this.Quantity = step.Quantity;
+            this.Time = step.Time;
+            this.Input = step.Input;
+        }
     }
 }
